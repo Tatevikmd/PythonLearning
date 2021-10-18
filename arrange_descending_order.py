8f0ed8303ee5b95364894017a3f0d1c3dd7494cd
Tatevikmd <91789563+Tatevikmd@users.noreply.github.com>
2021-10-19 00:41:29 +0400

    Add files via upload

---
+++ arrange_descending_order.py
@@ -0,0 +1,12 @@
+l=[1,3,2,4,5]                    
+i=0
+while(i< len(l)-1):
+    j=i
+    while (j<len(l)-1):
+        if(l[i]<l[j+1]):
+            a=l[j+1]
+            l[j+1]=l[i]
+            l[i]=a
+        j=j+1
+    i=i+1
+print(l)
